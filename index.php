7a69972ffe4470f7829282139b0611414d36125d
Andriy Bohdan <a.bohdan@gmail.com>
2019-10-31 17:32:29 +0200

    Update index.php

--- index.php
+++ index.php
@@ -24,33 +24,38 @@ if (!$ip && isset($argv[1])) {
 // Replace "city" with the appropriate method for your database, e.g.,
 // "country".
 
-print("<pre>");
+print("<pre>\n\n");
 try {
   $record = $ip ? $reader->city($ip) : null;
   // $record = $reader->city('46.149.80.233');
   if ($record) {
 
+    // var_export($record);
+
     print("IP: " .$ip . "\n"); // 'US'
     print("Country code: " .$record->country->isoCode . "\n"); // 'US'
     print("Country: " .$record->country->name . "\n"); // 'United States'
     // print($record->country->names['zh-CN'] . "\n"); // '美国'
-
-    print("Subdivision: " .$record->mostSpecificSubdivision->name . "\n"); // 'Minnesota'
-    print("Subdivision Code: " .$record->mostSpecificSubdivision->isoCode . "\n"); // 'MN'
+    
+    // var_export($record->subdivisions);
+    print("Subdivisions: " . implode(', ',array_map(function ($i) { return $i->name; }, $record->subdivisions))."\n" );
+    print("Subdivision Codes: " . implode(', ',array_map(function ($i) { return $i->isoCode; }, $record->subdivisions))."\n" );
+    print("Most specific subdivision: " .$record->mostSpecificSubdivision->name . "\n"); // 'Minnesota'
+    print("Most specific subdivision Code: " .$record->mostSpecificSubdivision->isoCode . "\n"); // 'MN'
 
     print("City: " .$record->city->name . "\n"); // 'Minneapolis'
 
-    print($record->postal->code . "\n"); // '55455'
+    print("Postal: " .$record->postal->code . "\n"); // '55455'
 
-    print($record->location->latitude . "\n"); // 44.9733
-    print($record->location->longitude . "\n"); // -93.2323
+    print("Lat: " .$record->location->latitude . "\n"); // 44.9733
+    print("Lng: " .$record->location->longitude . "\n"); // -93.2323
 
-    print($record->traits->network . "\n"); // '128.101.101.101/32'
+    // print("Network: " .$record->traits->network . "\n"); // '128.101.101.101/32'
   } else {
-    print("Private or invalid IP. Pass IP as query parameter ?ip=<IP>"); 
+    print("\nPrivate or invalid IP. Pass IP as query parameter ?ip=<IP>"); 
   }
 
 } catch (Exception $ex) {
-  print("Failure: ".$ex->getMessage()); 
+  print("\nFailure: ".$ex->getMessage()); 
 }
-print("</pre>");
+print("\n</pre>");
